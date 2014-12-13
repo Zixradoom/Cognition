2c989b0f1c5eb8c1ea76591c32274b659ef75b07
Anthony J Simon <zixradoom@gmail.com>
2014-12-13 11:39:19 -0500

    Add a serching componenet so that the property file can pe pust in multiple locations.

--- src/main/java/com/s2d/cognition/SystemPropsLoader.java
+++ src/main/java/com/s2d/cognition/SystemPropsLoader.java
@@ -2,6 +2,8 @@ package com.s2d.cognition;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,44 +16,101 @@ import org.apache.logging.log4j.Logger;
 /**
  * The {@link SystemPropsLoader} allows an application programmer to
  * load a java properties file by specifying the file path in the 
- * "com.s2d.cognition.system.props" system property. This is used so that a large
+ * "com.s2d.cognition.system.props" system property or using a well known name for the file.
+ * This is used so that a large
  * number of properties do not have to be set on the command line at runtime.
  * @author Anthony J Simon
  */
 public final class SystemPropsLoader
 {
-	private static final Logger LOGGER = LogManager.getLogger ( SystemPropsLoader.class );
-	
-	public static final String COGNITION_SYSTEM_PROPS = "com.s2d.cognition.system.props";
-	
-	/**
-	 * Read the "com.s2d.cognition.system.props" {@link System} property
-	 * and load the {@link Properties} file specified in it. Create the new <code>Properties</code> object
-	 * with the current <code>System</code> <code>Properties</code> as the parent and set the new
-	 * <code>Properties</code> as the new <code>System</code> <code>Properties</code>
-	 */
-	public static void init ()
-	{
-		String propsFilePath = System.getProperty ( COGNITION_SYSTEM_PROPS );
-		if ( propsFilePath == null )
-		{
-			LOGGER.info ( "[{}] is not set, no system property to load",
-					COGNITION_SYSTEM_PROPS );
-		}
-		else
-		{
-			Path propPath = Paths.get ( propsFilePath ).toAbsolutePath ();
-			try ( InputStream inputStream = Files.newInputStream ( propPath ) )
-			{
-				Properties props = new Properties ( System.getProperties () );
-				props.load ( inputStream );
-				System.setProperties ( props );
-				LOGGER.info ( "Cognition properties loaded from [{}]", propPath );
-			}
-			catch ( IOException e )
-			{
-				LOGGER.catching ( Level.ERROR, e );
-			}
-		}
-	}
+  private static final Logger LOGGER = LogManager.getLogger ( SystemPropsLoader.class );
+
+  public static final String COGNITION_SYSTEM_PROPS = "com.s2d.cognition.system.props";
+  public static final String DEFAULT_TEST_FILE_NAME = "cognitionTest.properties";
+  public static final String DEFAULT_FILE_NAME = "cognition.properties";
+
+  /**
+   * Call {@link SystemPropsLoader#getPropertiesURL()} and if
+   * it is not null attempt to load the file it specifies.
+   * Create the new <code>Properties</code> object
+   * with the current <code>System</code> <code>Properties</code> as the parent and set the new
+   * <code>Properties</code> as the new <code>System</code> <code>Properties</code>
+   */
+  public static void init ()
+  {
+    URL propFileURL = getPropertiesURL ();
+    if ( propFileURL != null )
+    {
+      try ( InputStream inputStream = propFileURL.openStream () )
+      {
+        Properties props = new Properties ( System.getProperties () );
+        props.load ( inputStream );
+        System.setProperties ( props );
+        LOGGER.info ( "Cognition properties loaded from [{}]", propFileURL );
+      }
+      catch ( IOException e )
+      {
+        LOGGER.catching ( Level.ERROR, e );
+      }
+    }
+    else
+    {
+      LOGGER.info ( "No Cognition Properties File has been detected." );
+    }
+  }
+
+  /**
+   * 
+   * @return The URL of the cognition properties file or null if one can not be found
+   */
+  public static URL getPropertiesURL()
+  {
+    /*
+     * Search Order
+     * 1) System properties
+     * 2) cognitionTest.properties
+     * 3) cognition.properties
+     */
+
+    URL returnValue = null;
+    String sysProp = System.getProperty ( COGNITION_SYSTEM_PROPS );
+    if ( sysProp != null && !sysProp.isEmpty () )
+    {
+      // try to get a url first
+      URL url = null;
+      try
+      {
+        url = new URL ( sysProp );
+      }
+      catch ( MalformedURLException e )
+      {
+        LOGGER.catching ( e );
+      }
+
+      // try to get path and covert to url
+      if ( url == null )
+      {
+        try
+        {
+          Path propPath = Paths.get ( sysProp );
+          propPath = propPath.toAbsolutePath ();
+          if ( Files.exists ( propPath ) && !Files.isDirectory ( propPath ) )
+            url = propPath.toUri ().toURL ();
+        }
+        catch ( MalformedURLException e )
+        {
+          LOGGER.catching ( e );
+        }
+      }
+      returnValue = url;
+    }
+
+    if ( returnValue == null )
+      returnValue = Thread.currentThread ().getContextClassLoader ().getResource ( DEFAULT_TEST_FILE_NAME );
+
+    if ( returnValue == null )
+      returnValue = Thread.currentThread ().getContextClassLoader ().getResource ( DEFAULT_FILE_NAME );
+
+    return returnValue;
+  }
 }
